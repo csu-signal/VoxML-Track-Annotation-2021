46659eb16951bd9de03c5b7ccd9f4f6e7e5794f7
mogullapallishivani <mogullapallishivani@gmail.com>
2021-02-25 13:34:36 -0500

    Added Images and UI updates

---
+++ config.js
@@ -0,0 +1,33 @@
+import Firebase from "firebase";
+
+// Optionally import the services that you want to use
+//import "firebase/auth";
+//import "firebase/database";
+//import "firebase/firestore";
+//import "firebase/functions";
+//import "firebase/storage";
+
+// Initialize Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyA-U2MS5PCVataASGhSzPIoFgZOyg-I5ug",
+    authDomain: "voxml-track-annotation-1b212.firebaseapp.com",
+    projectId: "voxml-track-annotation-1b212",
+    storageBucket: "voxml-track-annotation-1b212.appspot.com",
+    messagingSenderId: "440515005707",
+    appId: "1:440515005707:web:8f57f4a70c0dbd6a17660e",
+    measurementId: "G-R4FYSFKK9M"
+  };
+
+
+export function fbInitializeApp() {
+    return Firebase.initializeApp(firebaseConfig);
+}
+
+var db;
+export function fbAuthenticated() {
+  db = firebaseapp.database();
+}
+
+export function getfirebasedb() {
+  return db;
+}
