15fad69ff4830e691168a429590c9a4fdba997e1
mogullapallishivani <mogullapallishivani@gmail.com>
2021-02-28 14:30:27 -0500

    UI Updates

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ import {TextInput, Button} from 'react-native-paper';
 import {fbInitializeApp,getfirebasedb,fbAuthenticated} from "./config";
 
 var firebase;
+var objectsArray = [];
+var depictedActivityArray = [];
 class  App extends Component {
 
   constructor(props) {
@@ -25,20 +27,32 @@ render(){
   var objects = [];
 	for(let i = 0; i < this.state.count; i++){
 		objects.push(
+      <>
 			<View  style={{
-        flexDirection: 'row'}} key = {i}>
+        flexDirection: 'row',margin:5}} key = {i}>
 				<TextInput
+          style={{margin:20}}
           mode='flat'
           label="Object"
+          onChangeText={(text)=>{objectsArray[i]=text}}
         />
-        <Button icon="plus" mode="contained" 
-        onPress={() =>{
-        this.setState(prevState => {
-        return {count: prevState.count + 1} }) }}>
-        </Button>
+        <TextInput
+        style={{margin:20}}
+        mode='flat'
+        label="Depicted Activity"
+        onChangeText={(text)=>{depictedActivityArray[i]=text}}
+        />
 			</View>
+      <Button icon="plus" mode="contained"  color="blue" style={{width:50,height:50,alignContent:'center'}}
+      onPress={() =>{
+      this.setState(prevState => {
+      return {count: prevState.count + 1} }) }}>
+      </Button>
+      </>
 		)
 	}
+
+
   return (
     
     <View style={styles.container}>
@@ -46,7 +60,7 @@ render(){
       <Image style={styles.tinyLogo} source={require('./Images/'+1+'.jpg')} />
       <TextInput
         mode='flat'
-        label="Caption"
+        label= "caption"
         onChangeText={(text)=>{
           this.setState({captionText: text})
         }}
@@ -60,20 +74,43 @@ render(){
       />
       <View >
         { objects }
-        <Button  mode="contained" onPress={
+        <Text style={styles.headerText}>Change in circumstances</Text>
+ 
+         { objectsArray.map((item, key)=>(
+           	<View  style={{
+              flexDirection: 'row',margin:5}}>
+         <Text key={key} style={styles.TextStyle}> To {depictedActivityArray[key]} { objectsArray[key] } must </Text>
+        
+         <TextInput
+          mode='flat'
+          label="Action"
+         
+        />
+        </View>
+
+
+         )
+         )}
+        
+      </View>
+      <View>
+      <Button  mode="contained" onPress={
           ()=>{
             getfirebasedb().ref('/users/1232')
             .set({
               Caption: this.state.captionText,
               FocusActivitiy: this.state.FocusActivityText,
+              Objects: JSON.stringify(objectsArray),
             })
             .then(() => console.log('Data set.'));
           }
         }>
         Submit
         </Button>
+        <Button  mode="contained">
+        Next
+        </Button>
       </View>
-      {console.log("Hello world")}
       <StatusBar style="auto" />
     </View>
   );
@@ -83,6 +120,10 @@ export default App;
 const styles = StyleSheet.create({
   titleText: {
     fontSize: 40,
+    fontWeight: 'bold',
+  },
+  headerText: {
+    fontSize: 20,
     fontWeight: 'bold',
   },
   container: {
@@ -95,4 +136,9 @@ const styles = StyleSheet.create({
     width: 400,
     height: 400,
   },
+  TextStyle:{
+    fontSize : 25,
+     textAlign: 'center',
+     alignContent:'center'
+  }
 });
