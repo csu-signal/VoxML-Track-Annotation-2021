e9a186bdf51b3a9492a6c743f3e1cab1418e6f01
mogullapallishivani <mogullapallishivani@gmail.com>
2021-03-06 18:30:25 -0500

    Clear form on next  button click

--- App.js
+++ App.js
@@ -1,16 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import React,{Component} from 'react';
+import React,{Component,createRef} from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
-import {TextInput, Button} from 'react-native-paper';
+import {TextInput, Button,HelperText} from 'react-native-paper';
 import {getfirebasedb,fbAuthenticated} from "./config";
+import update from 'react-addons-update';
 
-var objectsArray = [];
-var depictedActivityArray = [];
-var changeInCircumstancesArray = [];
-var spatialActivitiesArray = [];
 class  App extends Component {
 
   constructor(props) {
+    
     super(props);
     this.state = {
       image_id:0,
@@ -18,9 +16,13 @@ class  App extends Component {
       spatialActivitiesCount:1,
       captionText:'',
       FocusActivityText:'',
-      objectsText:[],
-      activityText:[],
+      objectsText:[''],
+      activityText:[''],
+      changeInCircumstancesText:[''],
+      spatialActivityText:[''],
+      onSubmit:false,
     };
+    
   }
 
   componentDidMount() {
@@ -28,6 +30,18 @@ class  App extends Component {
     fbAuthenticated();
   }
 
+renameLabel=(i)=> {
+  var actText = this.state.activityText[i]?this.state.activityText[i]:"Activity";
+  var objText = this.state.objectsText[i]?this.state.objectsText[i]:"Object";
+  return ("To "+actText+", "+objText+" must");
+}
+
+hasErrors = (text) => {
+    if(!text && this.state.onSubmit){
+      return true;
+    }
+    return false;
+};
 
 render(){
 
@@ -38,34 +52,65 @@ render(){
       <>
 			<View  style={{
         flexDirection: 'row',margin:5,}} key = {i}>
+          <>
 				<TextInput
           style={{width:250,margin:20}}
           mode='flat'
           label="Object"
           onChangeText={(text)=>{
-            this.setState({
-                objectsText: [...this.state.objectsText, text]
-            })
-            objectsArray[i]=text
+            this.setState(update(this.state, {
+	                objectsText: {
+		                          [i]: {
+			                            $set: text
+	                          	  }
+	                            }
+          }));
+          
           }}
+          value = {this.state.objectsText[i]}
         />
+        <HelperText type="error" visible={this.hasErrors(this.state.objectsText[i])}>
+              Empty Text
+        </HelperText>
+        </>
         <TextInput
         style={{width:250,margin:20}}
         mode='flat'
         label="Associated Depicted Activity"
         onChangeText={(text)=>{
-          this.setState({
-                objectsText: [...this.state.objectsText, text]
-            })
-          depictedActivityArray[i]=text
+          this.setState(update(this.state, {
+	                activityText: {
+		                          [i]: {
+			                            $set: text
+	                          	  }
+	                            }
+          }));
         }}
+        value = {this.state.activityText[i]}
         />
+        <HelperText type="error" visible={this.hasErrors(this.state.activityText[i])}>
+        Empty Text
+        </HelperText>
         <TextInput
         style={{width:250,margin:20}}
         mode='flat'
-        label={"To "+this.state.activityText[i]+", "+this.state.objectsText[i]+" must"} 
-        onChangeText={(text)=>{depictedActivityArray[i]=text}}
+        label={
+            this.renameLabel(i)
+        }
+        onChangeText={(text)=>{
+          this.setState(update(this.state, {
+	                changeInCircumstancesText: {
+		                          [i]: {
+			                            $set: text
+	                          	  }
+	                            }
+          }));
+        }}
+        value = {this.state.changeInCircumstancesText[i]}
         />
+        <HelperText type="error" visible={this.hasErrors(this.state.changeInCircumstancesText[i])}>
+        Empty Text
+        </HelperText>
         <Button mode="contained"  
         color="#457b9d" 
         style={{margin:20,alignContent:'center',justifyContent: 'center'}}
@@ -85,14 +130,20 @@ render(){
       <>
 			<View  style={{
         flexDirection: 'row',margin:5,}} key = {i}>
-
 				<TextInput
         style={{width:500,margin:20}}
         mode='flat'
         label="Spatial Activity"
         onChangeText={(text)=>{
-          spatialActivitiesArray[i] = text;
+          this.setState(update(this.state, {
+	                spatialActivityText: {
+		                          [i]: {
+			                            $set: text
+	                          	  }
+	                            }
+          }));
         }}
+         value = {this.state.spatialActivityText[i]}
       />
         <Button mode="contained"  
         color="#457b9d" 
@@ -115,12 +166,15 @@ render(){
       <Text style={styles.titleText}>VoxML Annotation Survey</Text>
       <Image style={styles.tinyLogo} source={require('./Images/'+this.state.image_id+'.jpg')} />
       <TextInput
+        // ref={(ref) => this.mainInput= ref}
+        ref={input => { this.textRef = input }}
         style={{width:500,margin:20}}
         mode='flat'
         label= "Caption"
         onChangeText={(text)=>{
           this.setState({captionText: text})
         }}
+        value={this.state.captionText}
       />
       <TextInput
         style={{width:500,margin:20}}
@@ -129,6 +183,7 @@ render(){
         onChangeText={(text)=>{
           this.setState({FocusActivityText: text})
         }}
+        value={this.state.FocusActivityText}
       />
       <View >
         { objects }
@@ -140,13 +195,17 @@ render(){
         style={{margin:20,alignContent:'center',justifyContent: 'center'}}
         onPress={
           ()=>{
+            this.setState({
+              onSubmit:true,
+            })
             getfirebasedb().ref('/Image/'+this.state.image_id)
             .set({
               Caption: this.state.captionText,
               FocusActivitiy: this.state.FocusActivityText,
-              Objects: JSON.stringify(objectsArray),
-              depictedActivity: JSON.stringify(depictedActivityArray),
-              changeInCircumstances: JSON.stringify(changeInCircumstancesArray),
+              Objects: JSON.stringify(this.state.objectsText),
+              DepictedActivity: JSON.stringify(this.state.activityText),
+              ChangeInCircumstances: JSON.stringify(this.state.changeInCircumstancesText),
+              SpatialActivities: JSON.stringify(this.state.spatialActivityText),
             })
             .then(() => console.log('Data set.'));
           }
@@ -155,12 +214,23 @@ render(){
         </Button>
         <Button  mode="contained"  
         color="#457b9d"
-        onPress={()=>{
-          objectsArray = [];
-          depictedActivityArray = [];
-          changeInCircumstancesArray = [];
-          this.setState({image_id: Math.floor(Math.random() * Math.floor(105))})
-        }}
+        onPress={
+          ()=>{
+            
+          this.setState({
+            image_id: Math.floor(Math.random() * Math.floor(105)),
+            objectCount:1,
+            spatialActivitiesCount:1,
+            captionText:'',
+            FocusActivityText:'',
+            objectsText:[''],
+            activityText:[''],
+            changeInCircumstancesText:[''],
+            spatialActivityText:['']
+          })
+          }
+        
+      }
         style={{margin:20,alignContent:'center',justifyContent: 'center'}}>
         Next
         </Button>
@@ -169,6 +239,8 @@ render(){
     </View>
   );
       }
+    
+  
 }
 export default App;
 const styles = StyleSheet.create({
