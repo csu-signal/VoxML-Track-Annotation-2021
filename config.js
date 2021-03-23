1dbe4bdc27215b707bf619aeda9790a94225933a
mogullapallishivani <mogullapallishivani@gmail.com>
2021-03-22 22:34:27 -0400

    firebase config Updated

--- config.js
+++ config.js
@@ -8,25 +8,25 @@ import Firebase from "firebase";
 //import "firebase/storage";
 
 // Initialize Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyA-U2MS5PCVataASGhSzPIoFgZOyg-I5ug",
-  authDomain: "voxml-track-annotation-1b212.firebaseapp.com",
-  databaseURL: "https://voxml-track-annotation-1b212-default-rtdb.firebaseio.com",
-  projectId: "voxml-track-annotation-1b212",
-  storageBucket: "voxml-track-annotation-1b212.appspot.com",
-  messagingSenderId: "440515005707",
-  appId: "1:440515005707:web:8f57f4a70c0dbd6a17660e",
-  measurementId: "G-R4FYSFKK9M"
-};
 // const firebaseConfig = {
-//   apiKey: "AIzaSyA8oO3RbSrlG_XKDXcunnWn95wweqLTuBA",
-//   authDomain: "voxml-track-annotation-657b3.firebaseapp.com",
-//   databaseURL: "https://voxml-track-annotation-657b3-default-rtdb.firebaseio.com",
-//   projectId: "voxml-track-annotation-657b3",
-//   storageBucket: "voxml-track-annotation-657b3.appspot.com",
-//   messagingSenderId: "181447757267",
-//   appId: "1:181447757267:web:950e0198a86663ccf87326"
+//   apiKey: "AIzaSyA-U2MS5PCVataASGhSzPIoFgZOyg-I5ug",
+//   authDomain: "voxml-track-annotation-1b212.firebaseapp.com",
+//   databaseURL: "https://voxml-track-annotation-1b212-default-rtdb.firebaseio.com",
+//   projectId: "voxml-track-annotation-1b212",
+//   storageBucket: "voxml-track-annotation-1b212.appspot.com",
+//   messagingSenderId: "440515005707",
+//   appId: "1:440515005707:web:8f57f4a70c0dbd6a17660e",
+//   measurementId: "G-R4FYSFKK9M"
 // };
+const firebaseConfig = {
+  apiKey: "AIzaSyA8oO3RbSrlG_XKDXcunnWn95wweqLTuBA",
+  authDomain: "voxml-track-annotation-657b3.firebaseapp.com",
+  databaseURL: "https://voxml-track-annotation-657b3-default-rtdb.firebaseio.com",
+  projectId: "voxml-track-annotation-657b3",
+  storageBucket: "voxml-track-annotation-657b3.appspot.com",
+  messagingSenderId: "181447757267",
+  appId: "1:181447757267:web:950e0198a86663ccf87326"
+};
 // const firebaseConfig = {
 //   apiKey: "AIzaSyA8oO3RbSrlG_XKDXcunnWn95wweqLTuBA",
 //   authDomain: "voxml-track-annotation-657b3.firebaseapp.com",
